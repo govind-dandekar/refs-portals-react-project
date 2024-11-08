661ace55ef9cd76495c4c88f139617da7a0e9a34
Govind Dandekar <govind@Govinds-Mini.lan>
2024-11-08 11:58:07 -0800

    added useRef to input element and removed extra state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,31 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  const nameInput = useRef();
+  
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleClick(){
+   setPlayerName(nameInput.current.value)
+   // reset input field
+   // this is imperative code though --
+   // directly instructing browser to set value to null
+   // React is not handling the interaction
+   // but is ok because value not connected to state
+   playerName.current.value = null;
+  }
+  
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {(playerName !== null) ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={nameInput} // connects input element to const set 
+                          // by useRef() & gives access to all props
+          type="text"     // remove value prop; ref manages value
+          required
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
